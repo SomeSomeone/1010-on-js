e94bea4fd6d960f72f5bd009af47adc3a36d3dc2
SomeSomeone <nice.gl_orl@ukr.net>
2016-03-02 20:47:14 +0200

    board and figures are placed in the center, change colors

--- first.js
+++ first.js
@@ -1,14 +1,28 @@
 function Board (){
 	this.class_name="board";
 	this.class_block="block";
+
 	this.figur_count=3;
 	this.blocks_count=10;
-	this.block_size=parseInt(($( document ).height()-200)/this.blocks_count);
-	this.block_border=parseInt(this.block_size/20);//need rewrite
+
 	
+	this.block_size=parseInt(($( document ).height()/5*3)/this.blocks_count);
+	this.block_border=parseInt(this.block_size/20);//need rewrite
+	this.border_radius=	parseInt(this.block_size/4);
+
+
+
 	this.full_block_size=this.block_size+this.block_border*2;
 	this.full_size=this.full_block_size*this.blocks_count;
 	
+	var help =parseInt(($( document ).width()-this.full_size)/2)
+	if (help<=250){
+		this.offset=0;
+	}else{
+		this.offset=help
+	}
+
+
 	this.points=0;
 	this.control_sum=[[],[]];
 	this.must_clean=[[],[]];
@@ -32,14 +46,15 @@ Board.prototype.restart_board = function(){
 Board.prototype.draw = function(){
 	for (var x = this.blocks_count-1 ; x >= 0; x--) {
 		for (var y =this.blocks_count-1 ; y >= 0; y--) {
-			var style='style="left:'+(this.full_block_size*x)+'px;top:'+(this.full_block_size*y)+'px;"';
+			var style='style="left:'+(this.offset+this.full_block_size*x)+'px;top:'+(this.full_block_size*y)+'px;"';
 			$("."+this.class_name).append('<div class="'+this.class_block+'" id="'+x+'_'+y+'" ' + style+'></div>');
 		};
 		this.control_sum[0][x]=0;//x
 		this.control_sum[1][x]=0;//y
 	};
 	$("."+this.class_block).css({width:this.block_size,height:this.block_size});
-	$("."+this.class_block).css("border", "gray solid "+this.block_border+"px");
+	$("."+this.class_block).css("border", "#BDBDBD solid "+this.block_border+"px");
+	$("."+this.class_block).css("border-radius", this.border_radius+"px");	
 };
 
 Board.prototype.can_input_figur = function(figur, start_x, start_y){
@@ -68,7 +83,8 @@ Board.prototype.can_input_any_figurs = function(figurs){
 					var update_color=$("#"+update_x+"_"+update_y).css("background-color");
 					if( update_x<0||update_x>=this.blocks_count||
 						update_x<0||update_x>=this.blocks_count||
-						update_color != 'rgb(255, 255, 255)'){
+						(update_color != 'rgb(255, 255, 255)'&&
+						update_color != 'rgb(206, 206, 206)')){
 						var result=false;
 					};
 				};
@@ -116,12 +132,12 @@ Board.prototype.clean = function(){
 	            for (var i = this.blocks_count-1; i >= 0; i--) {
 					if(x_or_y==0){
 
-						 $("#"+number_in_must_clean+"_"+i).animate({ backgroundColor: "#000000" }, 400)
+						 $("#"+number_in_must_clean+"_"+i).animate({ backgroundColor: "#BDBDBD" }, 400)
 						 $("#"+number_in_must_clean+"_"+i).animate({ backgroundColor: "#ffffff" }, 400)
 					//	$("#"+number_in_must_clean+"_"+i).css( "backgroundColor" , 'white');
 					};
 					if(x_or_y==1){
-						$("#"+i+"_"+number_in_must_clean).animate({ backgroundColor: "#000000" }, 400);
+						$("#"+i+"_"+number_in_must_clean).animate({ backgroundColor: "#BDBDBD" }, 400);
 						$("#"+i+"_"+number_in_must_clean).animate({ backgroundColor: "#ffffff" }, 400);
 					//;	$("#"+i+"_"+number_in_must_clean).css( "backgroundColor" , 'white');
 					}
@@ -153,16 +169,15 @@ Board.prototype.status = function(){
 function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
     var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
+    color = letters[Math.floor(Math.random() * 16)];
     return color;
 }
 
 function Figur (board,name){
 	this.class_name=name;
 	this.board=board;
-	this.block_size=board.block_size;
+	this.block_size=parseInt(board.full_block_size/1.5);
+	this.border_radius=parseInt(board.border_radius/1.5);
 	this.number;
 	this.kind=-1;
 	this.blocks=[];
@@ -170,6 +185,11 @@ function Figur (board,name){
 Figur.prototype.figurs_count=3;
 
 Figur.prototype.figurs=[]; 
+
+Figur.prototype.blocks_colors = [	"#F44336","#E91E63","#D500F9","#2196F3",
+									"#5C6BC0","#00BCD4","#009688","#18FFFF",
+									"#43A047","#00E676","#8BC34A","#D4E157",
+									"#FFEB3B","#FFC107","#FF5722","#FB8C00"]
 
 Figur.prototype.blocks_combimation	= 	[	[ [0,0] , [0,1] ] ,// | 2
 											[ [0,0] , [0,1] , [0,2] ] ,// | 3
@@ -207,15 +227,18 @@ Figur.prototype.draw = function(number){
 	this.figurs[number]=this;
 	this.number=number;
 
-	this.background_color=getRandomColor();
+	this.background_color=this.blocks_colors[Math.floor(Math.random() * this.blocks_colors.length) ];
 	
 	this.kind = Math.floor((Math.random() * this.blocks_combimation.length) );
 	this.blocks=this.blocks_combimation[this.kind];
-
+	
+	var offset_left=this.board.offset+this.board.full_size*((1/3)*number);
+	var offset_top=this.block_size;
+	
 	$(".figurs").append('<div class='+this.class_name+'></div>');
 
 	for (var i in this.blocks) {
-		var style='style="left:'+(this.board.full_size+this.block_size*this.blocks[i][0])+'px;top:'+this.block_size*this.blocks[i][1]+'px;"';
+		var style='style="left:'+parseInt((offset_left+this.block_size*this.blocks[i][0]))+'px;top:'+parseInt((offset_top+this.board.full_size+this.block_size*this.blocks[i][1]))+'px;"';
 		$("."+this.class_name).append('<div class="block_move" id="'+this.blocks[i][0]+'_'+this.blocks[i][1]+'_'+number+'" '+style+'></div>');
 	};
 
@@ -223,6 +246,7 @@ Figur.prototype.draw = function(number){
 	
 	$(".block_move").css({width:this.block_size,height:this.block_size});
 	$("."+this.class_name+" > .block_move").css("background-color",this.background_color)
+	$(".block_move").css("border-radius", this.border_radius+"px");	
 };
 
 Figur.prototype.is_need_generate = function(){
@@ -250,9 +274,17 @@ Figur.prototype.erase = function(){
 	delete Figur.prototype.figurs[number];
 };
 
+
+
+
+
+
+
 var board = new Board ();
 board.draw();
 Figur.prototype.generate(board);
+
+$("#points").css("left",board.full_size+board.offset+board.full_block_size+'px' )
 
 $(function() {
 	$('.block').droppable({
@@ -275,7 +307,7 @@ $(function() {
 	            }
 	            //figur.draw(figur.number);
 	        };
-        	$("#points").text("POINTS : "+board.points);
+        	$("#points > span").text(board.points);
 	        if(!board.can_input_any_figurs(Figur.prototype.figurs)){
 	        	$("#end_game").append("you can't input any figurs")
 	        }
