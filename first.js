012baf19232ed0486ee66c4e57a4d8821ebd339f
SomeSomeone <nice.gl_orl@ukr.net>
2016-04-10 12:44:33 +0300

    Second encounter

--- first.js
+++ first.js
@@ -5,17 +5,15 @@ var OPTIONS = {
 	"BlockSize"			: parseInt(($( document ).height()/15)),
 	"FullBlockSize"		: parseInt(($( document ).height()/15)+4)
 }
+OPTIONS["FullBoardSize"]=OPTIONS["FullBlockSize"]*OPTIONS["BoardSize"];
+
+if (parseInt(($( document ).width()-OPTIONS["FullBoardSize"])/2)>250) {
+	OPTIONS["Offset"]=parseInt(($( document ).width()-OPTIONS["FullBoardSize"])/2);
+}else{
+	OPTIONS["Offset"]=0;
+}
 
 function Board (){
-	this.full_block_size=OPTIONS["BlockSize"]+4;
-	this.full_size=this.full_block_size*OPTIONS["BoardSize"];
-	var help =parseInt(($( document ).width()-this.full_size)/2)
-	if (help<=250){
-		this.offset=0;
-	}else{
-		this.offset=help
-	}
-
 	this.points=0;
 	this.control_sum=[[],[]];
 	this.must_clean=[[],[]];
@@ -34,20 +32,17 @@ Board.prototype.restart_board = function(){
 	};
 }
 
-
-
 Board.prototype.draw = function(){
 	console.log($("."+OPTIONS["BlockClassName"]).outerHeight())
 	for (var x = OPTIONS["BoardSize"]-1 ; x >= 0; x--) {
 		for (var y =OPTIONS["BoardSize"]-1 ; y >= 0; y--) {
-			var style='style="left:'+(this.offset+OPTIONS["FullBlockSize"]*x)+'px;top:'+(OPTIONS["FullBlockSize"]*y)+'px;"';
+			var style='style="left:'+(OPTIONS["Offset"]+OPTIONS["FullBlockSize"]*x)+'px;top:'+(OPTIONS["FullBlockSize"]*y)+'px;"';
 			$("."+OPTIONS["BoardClassName"]).append('<div class="'+OPTIONS["BlockClassName"]+'" id="'+x+'_'+y+'" ' + style+'></div>');
 		};
 		this.control_sum[0][x]=0;//x
 		this.control_sum[1][x]=0;//y
 	};
 	$("."+OPTIONS["BlockClassName"]).css({width:OPTIONS["BlockSize"],height:OPTIONS["BlockSize"]});
-
 };
 
 Board.prototype.can_input_figur = function(figur, start_x, start_y){
@@ -67,7 +62,6 @@ Board.prototype.can_input_figur = function(figur, start_x, start_y){
 };
 
 Board.prototype.can_input_any_figurs = function(figurs){
-
 	for(var figur in figurs){
 		for (var x =OPTIONS["BoardSize"]; x >= -2; x--) {
 			for (var y = OPTIONS["BoardSize"]; y >= -2; y--) {
@@ -78,7 +72,6 @@ Board.prototype.can_input_any_figurs = function(figurs){
 		};
 	};
 	return false;
-	
 };
 
 
@@ -145,22 +138,12 @@ Board.prototype.status = function(){
 	console.log('must_clean.y='+this.must_clean[1].join(" "));
 	console.log('control_sum.x='+this.control_sum[0].join(" "));
 	console.log('control_sum.x='+this.control_sum[1].join(" "));
-
-}
-
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    color = letters[Math.floor(Math.random() * 16)];
-    return color;
 }
 
 function Figur (board,name){
 	this.class_name=name;
 	this.board=board;
-	this.block_size=parseInt(OPTIONS["FullBlockSize"]/1.5);
-	this.border_radius=parseInt(OPTIONS["FullBlockSize"]/6);
+	this.block_size=parseInt(OPTIONS["FullBlockSize"]/1.3);
 	this.number;
 	this.kind=-1;
 	this.blocks=[];
@@ -174,7 +157,8 @@ Figur.prototype.blocks_colors = [	"#F44336","#E91E63","#D500F9","#2196F3",
 									"#43A047","#00E676","#8BC34A","#D4E157",
 									"#FFEB3B","#FFC107","#FF5722","#FB8C00"]
 
-Figur.prototype.blocks_combimation	= 	[	[ [2,1] , [2,2] ] ,// | 2
+Figur.prototype.blocks_combimation	= 	[	
+											[ [2,1] , [2,2] ] ,// | 2
 											[ [2,1] , [2,2] , [2,3] ] ,// | 3
 											[ [2,0] , [2,1] , [2,2] , [2,3] ] ,// | 4
 											[ [2,0] , [2,1] , [2,2] , [2,3] , [2,4] ] ,// | 5			 
@@ -215,13 +199,13 @@ Figur.prototype.draw = function(number){
 	this.kind = Math.floor((Math.random() * this.blocks_combimation.length) );
 	this.blocks=this.blocks_combimation[this.kind];
 	
-	var offset_left=this.board.offset+this.board.full_size*((1/3)*this.number);
+	var offset_left=OPTIONS["Offset"]+OPTIONS["FullBoardSize"]*((1/3)*this.number);
 	var offset_top=this.block_size;
 	
 	$(".figurs").append('<div class='+this.class_name+'></div>');
 
 	for (var i in this.blocks) {
-		var style='style="left:'+parseInt((offset_left+this.block_size*this.blocks[i][0]))+'px;top:'+parseInt((offset_top+this.board.full_size+this.block_size*this.blocks[i][1]))+'px;"';
+		var style='style="left:'+parseInt((offset_left+this.block_size*this.blocks[i][0]))+'px;top:'+parseInt((offset_top+OPTIONS["FullBoardSize"]+this.block_size*this.blocks[i][1]))+'px;"';
 		$("."+this.class_name).append('<div class="block_move" id="'+this.blocks[i][0]+'_'+this.blocks[i][1]+'_'+this.number+'" '+style+'></div>');
 	};
 
@@ -229,30 +213,28 @@ Figur.prototype.draw = function(number){
 	
 	$("."+this.class_name+" > .block_move").css({width:this.block_size,height:this.block_size});
 	$("."+this.class_name+" > .block_move").css("background-color",this.background_color)
-	$("."+this.class_name+" > .block_move").css("border-radius", this.border_radius+"px");	
 };
 
 Figur.prototype.upset=function(){
-	var offset_left=this.board.offset+this.board.full_size*((1/3)*this.number);
+	var offset_left=OPTIONS["Offset"]+OPTIONS["FullBoardSize"]*((1/3)*this.number);
 	var offset_top=this.block_size;
 	for (var i in this.blocks) {
 		$("#"+this.blocks[i][0]+'_'+this.blocks[i][1]+'_'+this.number).css({	left:		parseInt(offset_left+this.board.block_size*this.blocks[i][0]),
-																				top:		parseInt(offset_top+this.board.full_size+this.board.block_size*this.blocks[i][1])});
+																				top:		parseInt(offset_top+OPTIONS["FullBoardSize"]+this.board.block_size*this.blocks[i][1])});
 
 	};
 	$("."+this.class_name+" > .block_move").css({width:this.board.block_size,height:this.board.block_size})
 }
 
 Figur.prototype.downset=function(){
-	var offset_left=this.board.offset+this.board.full_size*((1/3)*this.number);
+	var offset_left=OPTIONS["Offset"]+OPTIONS["FullBoardSize"]*((1/3)*this.number);
 	var offset_top=this.block_size;
 	for (var i in this.blocks) {
 		$("#"+this.blocks[i][0]+'_'+this.blocks[i][1]+'_'+this.number).css({	left:		parseInt(offset_left+this.block_size*this.blocks[i][0]),
-																				top:		parseInt(offset_top+this.board.full_size+this.block_size*this.blocks[i][1])});
+																				top:		parseInt(offset_top+OPTIONS["FullBoardSize"]+this.block_size*this.blocks[i][1])});
 	};
 	$("."+this.class_name+" > .block_move").css({width:this.block_size,height:this.block_size})
 }
-
 
 Figur.prototype.is_need_generate = function(){
 	for(var i in Figur.prototype.figurs){
@@ -271,7 +253,6 @@ Figur.prototype.generate = function(board){
 	};
 }
 
-
 Figur.prototype.erase = function(){
     $('.'+this.class_name).remove();
 	var number=this.number;
@@ -279,26 +260,64 @@ Figur.prototype.erase = function(){
 	delete Figur.prototype.figurs[number];
 };
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    color = letters[Math.floor(Math.random() * 16)];
+    return color;
+}
 
 
+$(document).ready(function() {
 
-	$(document).ready(function() { 
+	function end_game(){ 
+	$('#overlay').fadeIn(400,
+	 	function(){
+	 		var record;
+	 		try{
+	 			record=localStorage.getItem("1010-record");
+	 		}
+	 		catch(e){
+	 			record=0;
+	 		}
+	 		$('#best_result_points').text(record)
+			$('#result_in_game').css('display', 'block').animate({opacity: 1, top: '50%'}, 200);
+			console.log(record);
+			console.log(1);
+			if(record < parseInt($('#result_points').text())){
+				localStorage.setItem("1010-record", parseInt($('#result_points').text()));
+				console.log(1);
+			};
 
+	});
+	};
+
+	function restart(){
+		$("#points>span , #result_points").text("0");
+		board.restart_board();
+		for(var i in Figur.prototype.figurs){
+			var a= Figur.prototype.figurs[i].erase();
+		}
+
+		if(Figur.prototype.is_need_generate()){
+			Figur.prototype.generate(board);
+		}
+	}
 
 	var board = new Board ();
 	board.draw();
+
 	Figur.prototype.generate(board);
 
-	$("#points").css({	left:		board.full_size+board.offset+OPTIONS["FullBlockSize"],
+	$("#points").css({	left:		OPTIONS["FullBoardSize"]+OPTIONS["Offset"]+OPTIONS["FullBlockSize"],
 						top:		OPTIONS["FullBlockSize"]
 					});
-	$("#restart").css({	left:		board.full_size+board.offset+OPTIONS["FullBlockSize"],
+	$("#restart").css({	left:		OPTIONS["FullBoardSize"]+OPTIONS["Offset"]+OPTIONS["FullBlockSize"],
 						top:		OPTIONS["FullBlockSize"]*3
 					});
 
-
 	$(function() {
-		$('.block').droppable({
+		$('.'+OPTIONS["BlockClassName"]).droppable({
 
 			drop: function(event, ui) {
 	        	var figur=Figur.prototype.figurs[event.target.id.split('_')[2]]
@@ -319,7 +338,6 @@ Figur.prototype.erase = function(){
 		            }
 		        };
 	        	$("#points > span , #result_points").text(board.points);
-	        	
 	        	setTimeout(function(){
 		        	if(!board.can_input_any_figurs(Figur.prototype.figurs)){
 			        	end_game();
@@ -327,34 +345,8 @@ Figur.prototype.erase = function(){
 	        	},2000);
 
 	        }
-	    });
-			
+	    });	
 	});
-
-
-	function end_game(){ 
-		$('#overlay').fadeIn(400,
-		 	function(){
-		 		var record;
-		 		try{
-		 			record=localStorage.getItem("1010-record");
-		 		}
-		 		catch(e){
-		 			record=0;
-		 		}
-		 		$('#best_result_points').text(record)
-				$('#result_in_game').css('display', 'block').animate({opacity: 1, top: '50%'}, 200);
-				console.log(record);
-				console.log(1);
-				if(record < parseInt($('#result_points').text())){
-					localStorage.setItem("1010-record", parseInt($('#result_points').text()));
-					console.log(1);
-				};
-
-		});
-	};
-
-
 	$('#overlay , .button_block').click( function(){
 			$('#result_in_game').animate({opacity: 0, top: '45%'},
 				200, 
@@ -365,22 +357,7 @@ Figur.prototype.erase = function(){
 			);
 			restart();
 	})
-			
-	function restart(){
-		$("#points>span , #result_points").text("0");
-		board.restart_board();
-		for(var i in Figur.prototype.figurs){
-			var a= Figur.prototype.figurs[i].erase();
-		}
-
-	    if(Figur.prototype.is_need_generate()){
-	    	Figur.prototype.generate(board);
-	    }
-	}
-
-
-		$('#restart').click(function(){
-			end_game();
-		});
+	$('#restart').click(function(){
+		end_game();
+	});
 });
-
